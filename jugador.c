c1894a76d2db60fcde6763b462660171b5745766
RoParadiso <66187087+RoParadiso@users.noreply.github.com>
2020-06-15 21:17:08 -0300

    Update jugador.c

--- jugador.c
+++ jugador.c
@@ -5,10 +5,10 @@ const int DEF_ANIMO_JUGADOR = 0;
 const char DEF_NIVEL_CONFIANZA_JUGADOR = 'w';
 const float DEF_FUERZA_PSIQUICA = -1.0F;
 
-int recibir_animo();
+/* int recibir_animo();
 char recibir_confianza();
 float recibir_fuerza_psiquica();
-
+*/
 
 int main(){
 
@@ -19,13 +19,13 @@ int main(){
 	comenzar_enfrentamiento();
 
 	animo = recibir_animo();
-	printf("Animo: %i\n", &animo);
+	printf("Animo: %i\n", animo);
 
 	confianza = recibir_confianza();
-	printf("Confianza:  %c\n", &confianza);
+	printf("Confianza:  %c\n", confianza);
 
 	fuerza_psiquica = recibir_fuerza_psiquica();
-	printf("Fuerza psiquica = %f\n", &fuerza_psiquica);
+	printf("Fuerza psiquica = %f\n", fuerza_psiquica);
 
 	resultado_enfrentamiento();
 
